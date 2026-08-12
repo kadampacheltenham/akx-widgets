86cf29034043ccaa9c5b07a71c0acef9516f93c1
kadampacheltenham <admin@meditationincheltenham.org.uk>
2026-08-12 18:31:38 +0100

    Create maintenance-status.js

---
+++ maintenance-status.js
@@ -0,0 +1,1 @@
+<iframe src="https://cdn.jsdelivr.net/gh/kadampacheltenham/akx-widgets@main/residents-status-list.html" style="width:100%;height:640px;border:0" loading="lazy" title="Reported issues"></iframe>
